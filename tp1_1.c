c2ac5086914f3920723f4b980c43360781e686c0
MiguelAngelBusto <chetobustos@gmail.com>
2025-03-25 13:48:15 -0300

    Codigo para resolver ejercicio puntero

--- tp1_1.c
+++ tp1_1.c
@@ -1,5 +1,29 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola Mundo!");
+    printf("Hola Mundo!\n\n");
+
+    // Declarar una variable de tipo int
+    int variable = 42;
+    
+    // Declarar un puntero que apunte a la variable
+    int *puntero = &variable;
+    
+    // 1) Mostrar el contenido del puntero (valor al que apunta, es decir, la variable)
+    printf("Contenido del puntero: %d\n", *puntero);
+    
+    // 2) Mostrar la dirección de memoria almacenada por el puntero
+    printf("Dirección de memoria almacenada por el puntero: %p\n", puntero);
+    
+    // 3) Mostrar la dirección de memoria de la variable
+    printf("Dirección de memoria de la variable: %p\n", &variable);
+    
+    // 4) Mostrar la dirección de memoria del puntero
+    printf("Dirección de memoria del puntero: %p\n", (void*)&puntero);
+    
+    // 5) Mostrar el tamaño de memoria utilizado por la variable usando sizeof()
+    printf("Tamaño de memoria de la variable: %zu bytes\n", sizeof(variable));
+    
+    return 0;
 }
+
