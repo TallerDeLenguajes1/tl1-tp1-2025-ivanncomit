67a1f28051a14674028cb1157f5743fc09e97146
ivanncomit <fabrivancj@gmail.com>
2025-04-13 16:09:01 -0300

    Definicion de funciones Cuadradas

--- tp1_2.c
+++ tp1_2.c
@@ -7,6 +7,15 @@ void orden(int numero1, int numero2);
 
 int main(){
     int a, b;
+    printf("Ingrese el primer valor: ");
+    scanf("%d", &a);
+    printf("La direccion de memoria del numero 1 es: %p", &a);
+    printf("\nLa variable tiene contenido: %d", a);
+    printf("\n\nIngrese el segundo valor: ");
+    scanf("%d", &b);
+    printf("La direccion de memoria del numero es: %p", &b);
+    printf("\nLa variable tiene contenido: %d", b);
+    
 
 
 
@@ -15,10 +24,13 @@ int main(){
 
 int CuadradodeN (int numero)
 {
-
+    int cuadrado = numero * numero;
+    return cuadrado;
 }
 void CuadDeNVoid (int numero)
 {
+    int cuadrado = numero * numero;
+    printf("El cuadrado del numero es: %d", cuadrado);
 
 }
 void Invertir(int numero1, int numero2)
@@ -27,5 +39,5 @@ void Invertir(int numero1, int numero2)
 }
 void orden(int numero1, int numero2)
 {
-    
+
 }
