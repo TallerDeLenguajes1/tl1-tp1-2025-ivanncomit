8867a08fb480b41e00c4a9030e68143c0023932b
ivanncomit <fabrivancj@gmail.com>
2025-04-13 14:11:05 -0300

    Declaracion de funciones e inicio del programa

---
+++ tp1_2.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int CuadradodeN (int numero);
+void CuadDeNVoid (int numero);
+void Invertir(int numero1, int numero2);
+void orden(int numero1, int numero2);
+
+int main(){
+    int a, b;
+
+
+
+    return 0;
+}
+
+int CuadradodeN (int numero)
+{
+
+}
+void CuadDeNVoid (int numero)
+{
+
+}
+void Invertir(int numero1, int numero2)
+{
+
+}
+void orden(int numero1, int numero2)
+{
+    
+}
